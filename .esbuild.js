333d3c7367e460ce15b97169ac623cfa2f2ef37d
shinich39 <shinich39@gmail.com>
2024-12-19 06:28:53 +0900

    update

--- .esbuild.js
+++ .esbuild.js
@@ -8,74 +8,90 @@ function camelize(str) {
   });
 }
 
-(async function () {
-  const MODULE_NAME = pkg.name.replace(/\W/g, "-").replace(/-?js$/, "");
-  const MODULE_VERSION = pkg.version;
-  const GLOBAL_NAME = camelize(MODULE_NAME); // iife
+const ENTRY_POINTS = ["index.js"];
+const MODULE_NAME = pkg.name.replace(/\W/g, "-").replace(/-?js$/, "");
+const MODULE_VERSION = pkg.version;
+const GLOBAL_NAME = camelize(MODULE_NAME); // For IIFE
 
-  if (fs.existsSync("./dist")) {
-    fs.rmSync("./dist", { recursive: true });
+pkg.main = `./dist/${MODULE_NAME}.min.mjs`;
+pkg.exports = {
+  ".": {
+    "import": `./dist/${MODULE_NAME}.min.cjs`,
+    "require": `./dist/${MODULE_NAME}.min.mjs`,
   }
+};
 
-  // ESM
-  // await esbuild.build({
-  //   entryPoints: ["index.js"],
-  //   bundle: true,
-  //   minify: false,
-  //   platform: "node",
-  //   format: "esm",
-  //   outfile: `dist/${MODULE_NAME}.mjs`,
-  // });
+const ESM = [
+  {
+    entryPoints: ENTRY_POINTS,
+    bundle: true,
+    minify: false,
+    platform: "node",
+    format: "esm",
+    outfile: `dist/${MODULE_NAME}.mjs`,
+  }, {
+    entryPoints: ENTRY_POINTS,
+    bundle: true,
+    minify: true,
+    platform: "node",
+    format: "esm",
+    outfile: `dist/${MODULE_NAME}.min.mjs`,
+  }
+];
 
-  // Minify ESM
-  // await esbuild.build({
-  //   entryPoints: ["index.js"],
-  //   bundle: true,
-  //   minify: true,
-  //   platform: "node",
-  //   format: "esm",
-  //   outfile: `dist/${MODULE_NAME}.min.mjs`,
-  // });
+const CJS = [
+  {
+    entryPoints: ENTRY_POINTS,
+    bundle: true,
+    minify: false,
+    platform: "node",
+    format: "cjs",
+    outfile: `dist/${MODULE_NAME}.cjs`,
+  }, {
+    entryPoints: ENTRY_POINTS,
+    bundle: true,
+    minify: true,
+    platform: "node",
+    format: "cjs",
+    outfile: `dist/${MODULE_NAME}.min.cjs`,
+  }
+];
 
-  // CommonJS
-  // await esbuild.build({
-  //   entryPoints: ["index.js"],
-  //   bundle: true,
-  //   minify: false,
-  //   platform: "node",
-  //   format: "cjs",
-  //   outfile: `dist/${MODULE_NAME}.cjs`,
-  // });
-
-  // Minify CJS
-  // await esbuild.build({
-  //   entryPoints: ["index.js"],
-  //   bundle: true,
-  //   minify: true,
-  //   platform: "node",
-  //   format: "cjs",
-  //   outfile: `dist/${MODULE_NAME}.min.cjs`,
-  // });
-
-  // Browser
-  await esbuild.build({
-    entryPoints: ["index.js"],
+const BROWSERIFY = [
+  {
+    entryPoints: ENTRY_POINTS,
     bundle: true,
     minify: false,
     platform: "browser",
     format: "iife",
     globalName: GLOBAL_NAME,
     outfile: `dist/${MODULE_NAME}.js`,
-  });
-
-  // Minify Browser
-  await esbuild.build({
-    entryPoints: ["index.js"],
+  }, {
+    entryPoints: ENTRY_POINTS,
     bundle: true,
     minify: true,
     platform: "browser",
     format: "iife",
     globalName: GLOBAL_NAME,
     outfile: `dist/${MODULE_NAME}.min.js`,
-  });
+  }
+];
+
+;(async function () {
+  if (fs.existsSync("./dist")) {
+    fs.rmSync("./dist", { recursive: true });
+  }
+
+  const options = [
+    // ...ESM,
+    // ...CJS,
+    ...BROWSERIFY,
+  ];
+
+  for (const option of options) {
+    await esbuild.build(option);
+  }
+
+  // Write package.json
+  fs.writeFileSync("package.json", JSON.stringify(pkg, null, 2), "utf8");
 })();
